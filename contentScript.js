a09c3cce8691cd93d694ee62ae18dc2ccacb77ed
Hamed Tahmasebi <87143540+hamedtahmasebi@users.noreply.github.com>
2022-07-17 00:11:40 +0430

    init

--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,48 @@
 (() => {
     let youtubeLeftControls, youtubePlayer;
     let currentVideo = "";
-    let currentVideoBookmarks = [];
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, value, videoId } = obj;
-
         if (type === "NEW") {
             currentVideo = videoId;
             newVideoLoaded();
         }
     });
-
     const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
-
+        const bookmarkBtnExists =
+            document.getElementsByClassName(".bookmark-btn")[0];
         if (!bookmarkBtnExists) {
             const bookmarkBtn = document.createElement("img");
 
             bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
+            bookmarkBtn.title = "Click to bookmark timestamp";
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubeLeftControls =
+                document.getElementsByClassName("ytp-left-controls")[0];
             youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
+
+            youtubeLeftControls.appendChild(bookmarkBtn);
+
             bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
         }
-    }
+    };
 
-    const addNewBookmarkEventHandler = () => {
+    newVideoLoaded();
+
+    const addNewBookmarkEventHandler = (e) => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
             desc: "Bookmark at " + getTime(currentTime),
         };
-        console.log(newBookmark);
+    };
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
+    const getTime = (t) => {
+        let date = new Date(0);
+        date.setSeconds(t);
 
-    newVideoLoaded();
+        return date.toISOString().substring(11, 8);
+    };
 })();
-
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
-
-    return date.toISOString().substr(11, 0);
-}
